5fef5e289125e78a2a5e056cfb0fca0d0efe5a7a
CuongGM <cuonggm48@gmail.com>
2022-08-20 15:19:12 +0700

    Add const.js styled-components

---
+++ src/const.js
@@ -0,0 +1,8 @@
+export const primaryColor = "#2F4454";
+export const primaryHoverColor = "#233441";
+export const secondaryColor = "#F4976C";
+export const secondaryHoverColor = "#E79068";
+export const darkColor = "#373E42";
+export const darkHoverColor = "#2E3437";
+export const lightColor = "#E7E7E7";
+export const lightHoverColor = "#D6D6D6";
